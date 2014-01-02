b00893397e4dd3de0696d9313014c76717457bf5
aded <aded@aded.it>
2014-01-02 16:37:47 +0100

    implemented methods to get account details, funds and app keys

--- account.go
+++ account.go
@@ -25,3 +25,79 @@
 // <http://www.gnu.org/licenses/>.
 
 package betfair
+
+import (
+	"encoding/json"
+	"strings"
+)
+
+// Response for Account details.
+type AccountDetailsResponse struct {
+	CurrencyCode	string
+	FirstName		string
+	LastName		string
+	LocaleCode		string
+	Region			string
+	Timezone		string
+	DiscountRate	float32
+	PointsBalance	int
+}
+
+// Response for retrieving available to bet.
+type AccountFundsResponse struct {
+	AvailableToBetBalance	float32
+	Exposure				float32
+	RetainedCommission		float32
+	ExposureLimit			float32
+}
+
+// Describes developer/vendor specific application.
+type DeveloperApp struct {
+	AppName		string
+	AppId		uint64
+	AppVersions	[]DeveloperAppVersion
+}
+
+// Describes a version of an external application.
+type DeveloperAppVersion struct {
+	Owner					string
+	VersionId				uint64
+	Version					string
+	ApplicationKey			string
+	DelayData				bool
+	SubscriptionRequired	bool
+	OwnerManaged			bool
+	Active					bool
+}
+
+// Get Account details.
+func (s *Session) GetAccountDetails() (AccountDetailsResponse, error) {
+	var response AccountDetailsResponse
+	err := doAccountRequest(s, "getAccountDetails", &response)
+	return response, err
+}
+
+// Get available to bet amount.
+func (s *Session) GetAccountFunds() (AccountFundsResponse, error) {
+	var response AccountFundsResponse
+	err := doAccountRequest(s, "getAccountFunds", &response)
+	return response, err
+}
+
+// Get all application keys owned by the given developer/vendor.
+func (s *Session) GetDeveloperAppKeys() ([]DeveloperApp, error) {
+	var response []DeveloperApp
+	err := doAccountRequest(s, "getDeveloperAppKeys", &response)
+	return response, err
+}
+
+func doAccountRequest(s *Session, method string, v interface{}) error {
+	data, err := doRequest(s, "account", method, strings.NewReader(""))
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(data, v); err != nil {
+		return err
+	}
+	return nil
+}
